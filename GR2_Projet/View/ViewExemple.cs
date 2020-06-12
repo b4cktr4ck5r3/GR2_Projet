b718035547f052d1d2e849304867f8539a568e02
b4cktr4ck5r3 <flennert@yahoo.fr>
2020-06-12 13:43:14 +0200

    commit test pour les dossiers du modèle MVC

---
+++ GR2_Projet/View/ViewExemple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GR2_Projet.View
+{
+    public partial class ViewExemple : UserControl
+    {
+        public ViewExemple()
+        {
+            InitializeComponent();
+        }
+    }
+}
