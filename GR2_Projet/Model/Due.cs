f9aebd183ea329ac602f16a442fd62d1b2b67605
Bakir75 <bakirait8@gmail.com>
2020-06-13 18:34:02 +0200

    Ajout classe Due

---
+++ GR2_Projet/Model/Due.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GR2_Projet.Model
+{
+    class Due
+    {
+        public int id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public DateTime date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public Due()
+        {
+
+        }
+
+
+
+    }
+}
