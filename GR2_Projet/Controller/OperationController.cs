62951d03a2765c8e98816ee0dcf12361a8c9ff2c
b4cktr4ck5r3 <flennert@yahoo.fr>
2020-06-13 17:16:48 +0200

    Création des classes Model + Controller des opérations bancaires.

---
+++ GR2_Projet/Controller/OperationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GR2_Projet.Controller
+{
+    /// <summary>
+    /// Controller du modèle opération.
+    /// </summary>
+    public class OperationController
+    {
+        /// <summary>
+        /// Constructeur.
+        /// </summary>
+        public OperationController() { }
+    }
+}
---
+++ GR2_Projet/Model/Operation.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GR2_Projet.Model
+{
+    /// <summary>
+    /// Enumération des types d'opérations disponibles.
+    /// </summary>
+    public enum operationType
+    {
+        Debit,
+        Credit
+    }
+
+    /// <summary>
+    /// Enumération des types de paiements disponibles.
+    /// </summary>
+    public enum paymentType
+    {
+        Card,
+        Transfer,
+        Cash,
+        Cheque
+    }
+
+    /// <summary>
+    /// Classe représentant le modèle d'une opération bancaire.
+    /// </summary>
+    public class Operation
+    {
+        /// <summary>
+        /// Propriété id.
+        /// </summary>
+        public int id
+        {
+            get { return id; } 
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// Propriété name.
+        /// </summary>
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Propriété type d'opération.
+        /// </summary>
+        public operationType operation_Type
+        {
+            get { return operation_Type; }
+            set { operation_Type = value; }
+        }
+
+        /// <summary>
+        /// Propriété date.
+        /// </summary>
+        public DateTime date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        /// <summary>
+        /// Propriété type de paiement.
+        /// </summary>
+        public paymentType payment_Type
+        {
+            get { return payment_Type; }
+            set { payment_Type = value; }
+        }
+
+        /// <summary>
+        /// Propriété montant.
+        /// </summary>
+        public int amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        /// <summary>
+        /// Constructeur.
+        /// </summary>
+        public Operation() { }
+    }
+}
