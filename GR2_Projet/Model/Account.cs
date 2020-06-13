2e19c10a4851a77e45d04ea61fe300f5e538540e
unknown <w.vatier@gmail.com>
2020-06-13 07:16:07 +0200

    Model Account

---
+++ GR2_Projet/Model/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GR2_Projet.Model
+{
+    class Account
+    {
+        public int id { get { return id; } set { id = value; } }
+        public string name { get { return name; } set { name = value; } }
+        public int budget { get { return budget; } set { budget = value; } }
+
+        public Account() { }
+    }
+}
