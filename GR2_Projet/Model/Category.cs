5cec98554468e611d0e69a1b9f71902100efad9a
jérémie Tr <elliosse@hotmail.fr>
2020-06-13 18:05:00 +0200

    Class Category

---
+++ GR2_Projet/Model/Category.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GR2_Projet.Model
+{
+    class Category
+    {
+        private int id;
+        private string name;
+
+        public Category(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+    }
+}
